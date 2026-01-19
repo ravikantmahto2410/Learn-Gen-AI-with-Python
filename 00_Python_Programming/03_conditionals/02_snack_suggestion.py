13147d08a38cf402c7036857e76f8d16c17444fb
Ravikant Mahto <157304763+ravikantmahto2410@users.noreply.github.com>
2026-01-19 16:48:54 +0530

    make a chai price calculator

---
+++ 00_Python_Programming/03_conditionals/02_snack_suggestion.py
@@ -0,0 +1,7 @@
+snack = input("Enter your preferred snack: ").lower()
+
+if snack == "cookies" or snack == "samosa":
+    print(f"Great Choice! We'll serve you {snack}")
+
+else:
+    print(f"Sorry we only serve cookies and Samosa with tea")    
