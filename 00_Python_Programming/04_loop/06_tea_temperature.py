9d183dde612aabfb5e44e484b017427d3e5d224b
Ravikant Mahto <157304763+ravikantmahto2410@users.noreply.github.com>
2026-02-04 15:36:25 +0530

    Learn upto the for else

---
+++ 00_Python_Programming/04_loop/06_tea_temperature.py
@@ -0,0 +1,9 @@
+temperature = 40
+
+while temperature < 100:
+    temperature = temperature + 15
+    print(f"Current temperature : {temperature}")
+    
+    # we can also write temperature = temperature + 15 as  temperature += 15
+
+print("Tea is ready to Boil")
---
+++ 00_Python_Programming/04_loop/07_out_of_order.py
@@ -0,0 +1,49 @@
+# flavours = ["Ginger", "Out of Stock", "Lemon", "Discontinued", "Tulsi"]
+
+# for flavour in flavours:
+#     if flavour == "Out of Stock":
+#         continue
+#     if flavour == "Discontinued":
+#         break
+#     print("Discontinued item found")
+
+# print("Outside  of loop")
+#########################################
+# flavours = ["Ginger", "Out of Stock", "Lemon", "Discontinued", "Tulsi"]
+
+# for flavour in flavours:
+#     if flavour == "Out of Stock":
+#         continue
+#     if flavour == "Discontinued":
+#         break
+#     print(f"{flavour} item found")
+
+# print("Outside  of loop")
+
+#######################################################
+
+# flavours = ["Ginger", "Out of Stock", "Lemon", "Discontinued", "Tulsi"]
+
+# for flavour in flavours:
+#     if flavour == "Out of Stock":
+#         continue
+#     if flavour == "Discontinued":
+#         print(f"{flavour} item found")
+#         break
+
+# print("Outside  of loop")
+
+
+################################################
+flavours = ["Ginger", "Out of Stock", "Lemon", "Discontinued", "Tulsi"]
+
+for flavour in flavours:
+    if flavour == "Out of Stock":
+        continue
+    if flavour == "Discontinued":
+        print(f"{flavour} item found")
+        break
+    print(f"{flavour} item found")
+
+print("Outside  of loop")
+
